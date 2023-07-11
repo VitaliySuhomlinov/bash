927454eab809dd6c1b9b721dd44b55d2a8839d56
VitaliySuhomlinov <scriptcraft.site@gmail.com>
2023-07-11 15:03:03 +0300

    11 june 2023

--- main.php
+++ main.php
@@ -1,8 +1,38 @@
 <?php
-echo 'one';
+class StoragePostIdForAmazon
+{
 
+	private static $instances;
+	private static $post_id;
 
+	protected function __construct() { }
 
-if (true) {
-    echo 'Ablamingo!';
+	protected function __clone() { }
+
+	public function __wakeup()
+	{
+    	throw new \Exception("Cannot unserialize a singleton.");
+	}
+
+	public static function getInstance(): StoragePostIdForAmazon
+	{
+    	if (!isset(self::$instances)) {
+        	self::$instances = new static();
+    	}
+
+    	return self::$instances;
+	}
+
+	public function setPostId($post_id)
+	{
+    	self::$post_id = $post_id;
+	}
+
+	public function getPostId()
+	{
+    	return self::$post_id;
+	}
+
 }
+
+
